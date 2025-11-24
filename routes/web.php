de810c4c7c9e27dcc4cecf3b317dcb77b7b011f3
Hardi Agunadi <zigunx01@gmail.com>
2025-11-24 22:47:51 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicKegiatanController;
+use App\Http\Controllers\KegiatanSuratController;
 
 //Route::get('/', function () {
 //    return view('audio');
