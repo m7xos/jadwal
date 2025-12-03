76de452e401730e001e454ef384539abfd50f3ea
Hardi Agunadi <zigunx01@gmail.com>
2025-12-03 14:21:20 +0700

    Fix Filament theme import path

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Sky,
             ])
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(
                 in: app_path('Filament/Resources'),
                 for: 'App\\Filament\\Resources',
