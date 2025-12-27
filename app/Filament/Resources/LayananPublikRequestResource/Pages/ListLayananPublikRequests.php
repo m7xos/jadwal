86db080082512e2050321adc6b601554fe09cfad
hardiagunadi <zigunx01@gmail.com>
2025-12-27 17:53:27 +0700

    tambah layanan

--- app/Filament/Resources/LayananPublikRequestResource/Pages/ListLayananPublikRequests.php
+++ app/Filament/Resources/LayananPublikRequestResource/Pages/ListLayananPublikRequests.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\LayananPublikRequestResource\Pages;
 
 use App\Filament\Resources\LayananPublikRequestResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListLayananPublikRequests extends ListRecords
 {
     protected static string $resource = LayananPublikRequestResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Tambah Register Layanan'),
+        ];
+    }
 }
--- app/Filament/Resources/LayananPublikResource/Pages/ListLayananPubliks.php
+++ app/Filament/Resources/LayananPublikResource/Pages/ListLayananPubliks.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\LayananPublikResource\Pages;
 
 use App\Filament\Resources\LayananPublikResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListLayananPubliks extends ListRecords
 {
     protected static string $resource = LayananPublikResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->label('Tambah Layanan Publik'),
+        ];
+    }
 }
