a37a3c998c0093e342b872aa450a88532aa9ab44
hardiagunadi <zigunx01@gmail.com>
2026-01-09 06:31:24 +0700

    foto profil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,10 +24,29 @@
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
+                        @php
+                            $user = auth()->user();
+                            $photoUrl = null;
+                            if (! empty($user?->photo_url)) {
+                                $photoUrl = $user->photo_url;
+                            } else {
+                                $nip = preg_replace('/\D+/', '', (string) ($user?->nip ?? ''));
+                                if ($nip !== '') {
+                                    $photoUrl = "https://simpeg.wonosobokab.go.id/packages/upload/photo/pegawai/{$nip}.jpg";
+                                }
+                            }
+                        @endphp
                         <a
                             href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                            class="inline-flex items-center gap-2 px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
+                            @if ($photoUrl)
+                                <img
+                                    src="{{ $photoUrl }}"
+                                    alt="Foto profil"
+                                    class="h-6 w-6 rounded-full object-cover border border-[#19140035] dark:border-[#3E3E3A]"
+                                >
+                            @endif
                             Dashboard
                         </a>
                     @else
